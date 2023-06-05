3af6b4961a5deb99d8e55e1fe7d6fea379cc4b97
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-06-05 18:21:02 +0600

    recent work section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,36 @@
 //       $(".mobile-menu").addClass("hidden");
 //     });
 //   });
+
+// swiper slider start 
+var swiper_recent_work = new Swiper(".recent_work", {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".recent_work_pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+  });
+// swiper slider end
